64eb02f2e415201d1b1980c0ed868da7e5781ab0
Crross <cristobalromerorossi@gmail.com>
2023-08-09 11:15:29 -0500

    chore: config root project to run dev environment

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import {Button} from "../components/index";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Button>Example button</Button>
+  </React.StrictMode>)
