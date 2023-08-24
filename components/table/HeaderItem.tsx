4385bfea2c2241cdfe1bf737f57cc8a22ae1ea1a
Kevin Soto <ingkevinsoto@gmail.com>
2023-08-23 21:42:04 -0500

    search input updated

--- components/table/HeaderItem.tsx
+++ components/table/HeaderItem.tsx
@@ -1,13 +1,13 @@
-import { useState } from "react"; // Importa useState
+import { useState } from "react";
 import styles from "./HeaderItem.module.scss";
 import { BodyText } from "../typography/BodyText";
 import { CaretDownOutlined, CaretUpOutlined, FilterFilled, SearchOutlined } from "@ant-design/icons";
 
 interface HeaderItemProps {
   title: string;
-  sorter?: boolean; // Agrega el prop sorter
-  filters?: boolean; // Agrega el prop filters
-  search?: boolean; // Agrega el prop search
+  sorter?: boolean;
+  filters?: boolean;
+  search?: boolean;
 }
 
 const SorterComponent = () => {
@@ -20,31 +20,33 @@ const SorterComponent = () => {
 };
 
 export const HeaderItem = ({ title, sorter, filters, search }: HeaderItemProps) => {
-  const [searchText, setSearchText] = useState(""); // Estado para el texto de búsqueda
+  const [searchText, setSearchText] = useState("");
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchText(e.target.value); // Actualizar el estado del texto de búsqueda
+    setSearchText(e.target.value);
   };
 
   return (
     <th className={styles.headerItem}>
       <div className={styles.headerItem__titleSection}>
         <BodyText size={"small"}>{title}</BodyText>
-        {sorter && <SorterComponent />}
-        {filters && <FilterFilled />}
-        {search && (
-          <>
-            <SearchOutlined />
-            <input
-              type="text"
-              value={searchText}
-              onChange={handleSearchChange} // Manejar cambios en el campo de búsqueda
-              className={styles.searchInput}
-              placeholder="Search"
-            />
-          </>
-        )}
+        <div className={styles.iconsContainer}>
+          {sorter && <SorterComponent />}
+          {filters && <FilterFilled />}
+          {search && <SearchOutlined />}
+        </div>
       </div>
+      {search && (
+        <div className={styles.searchContainer}>
+          <input
+            type="text"
+            value={searchText}
+            onChange={handleSearchChange}
+            className={styles.searchInput}
+            placeholder="Search"
+          />
+        </div>
+      )}
     </th>
   );
 };
