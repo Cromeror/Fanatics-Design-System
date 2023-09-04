f09a6e6e7b8ad7b89c732cbf4d7a0a0bf01090c3
Crross <cristobalromerorossi@gmail.com>
2023-09-04 09:16:00 -0500

    fix: row item

--- components/table/RowItem.tsx
+++ components/table/RowItem.tsx
@@ -1,17 +1,18 @@
 import * as React from "react";
 import styles from "./rowItem.module.scss";
+import {BodyText} from "../typography/BodyText";
+
 interface RowItemProps {
   children: React.ReactNode;
   selected?: boolean;
 }
 
-
-export const RowItem = ({ children, selected}: RowItemProps) => {
+export const RowItem = ({children, selected}: RowItemProps) => {
   return (
-      <tr className={`${styles.rowItem} ${selected ? styles.selected : ""}`}>
-        <td className={styles.rowItemCell}>{children}</td>
-      </tr>
+    <tr className={`${styles.rowItem} ${selected ? styles.selected : ""}`}>
+      <td className={styles.rowItemCell}>
+        <BodyText size={"small"}>{children}</BodyText>
+      </td>
+    </tr>
   );
 };
-
-export default RowItem;
