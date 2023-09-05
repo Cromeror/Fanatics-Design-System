a4f8fd47ebffc805f208fd6c17fd9e6db2992fb2
Kevin Soto <ingkevinsoto@gmail.com>
2023-09-05 00:42:00 -0500

    fixed props missing

--- documentation/utils/ShadowGrid.tsx
+++ documentation/utils/ShadowGrid.tsx
@@ -3,13 +3,68 @@ import { Col, Row } from "antd";
 import RectangleShadow from "./RectangleShadow";
 
 const shadowVariations = [
-  { title: "Evaluation Low", shadowType: "evaluationLow", hexCode: "#0000000a", opacity: 0.08 },
-  { title: "Evaluation Middle", shadowType: "evaluationMiddle", hexCode: "#0000000a", opacity: 0.04 },
-  { title: "Evaluation High", shadowType: "evaluationHigh", hexCode: "#00000014", opacity: 0.08 },
-  { title: "Focus Primary", shadowType: "focusPrimary", hexCode: "#e6f4ff", opacity: 0 },
-  { title: "Focus Error", shadowType: "focusError", hexCode: "#fff1f0", opacity: 0 },
-  { title: "Focus Warning", shadowType: "focusWarning", hexCode: "#fffbe6", opacity: 0 },
+  {
+    title: "Evaluation Low",
+    shadowType: "evaluationLow",
+    hexCode: "#0000000a",
+    opacity: 0.08,
+    x: 0,
+    y: 0,
+    blur: 4,
+    spread: 0,
+  },
+  {
+    title: "Evaluation Middle",
+    shadowType: "evaluationMiddle",
+    hexCode: "#0000000a",
+    opacity: 0.04,
+    x: 0,
+    y: 0,
+    blur: 8,
+    spread: 0,
+  },
+  {
+    title: "Evaluation High",
+    shadowType: "evaluationHigh",
+    hexCode: "#00000014",
+    opacity: 0.08,
+    x: 0,
+    y: 0,
+    blur: 16,
+    spread: 0,
+  },
+  {
+    title: "Focus Primary",
+    shadowType: "focusPrimary",
+    hexCode: "#e6f4ff",
+    opacity: 0,
+    x: 0,
+    y: 0,
+    blur: 0,
+    spread: 2,
+  },
+  {
+    title: "Focus Error",
+    shadowType: "focusError",
+    hexCode: "#fff1f0",
+    opacity: 0,
+    x: 0,
+    y: 0,
+    blur: 0,
+    spread: 2,
+  },
+  {
+    title: "Focus Warning",
+    shadowType: "focusWarning",
+    hexCode: "#fffbe6",
+    opacity: 0,
+    x: 0,
+    y: 0,
+    blur: 0,
+    spread: 2,
+  },
 ];
+
 
 export const ShadowGrid = () => {
   return (
@@ -21,6 +76,10 @@ export const ShadowGrid = () => {
             hexCode={variation.hexCode}
             shadowType={variation.shadowType}
             opacity={variation.opacity}
+            x={variation.x}
+            y={variation.y}
+            blur={variation.blur}
+            spread={variation.spread}
           />
         </Col>
       ))}
--- themes/shadows.ts
+++ themes/shadows.ts
@@ -8,25 +8,4 @@ export const shadows = {
   focusWarning: "0px 0px 0px 2px #fffbe6",
 
 
-
-  /*
-  evaluationLow: [{"x":0,"y":0,"blur":4,"spread":0,"color":"#000000 0.08","type":"dropShadow"},
-  {"x":0,"y":1,"blur":2,"spread":-1,"color":"#000000 0.04","type":"dropShadow"},
-  {"x":0,"y":2,"blur":7,"spread":2,"color":"#0000000 0.02","type":"dropShadow"}],
-
-  evaluationMiddle: [{"x":0,"y":0,"blur":8,"spread":0,"color":"#000000 0.04","type":"dropShadow"},
-  {"x":0,"y":2,"blur":3,"spread":-2,"color":"#000000 0.06","type":"dropShadow"},
-  {"x":0,"y":4,"blur":14,"spread":4,"color":"#000000 0.03","type":"dropShadow"}],
-
-  evaluationHigh: [{"x":0,"y":0,"blur":16,"spread":0,"color":"#000000 0.08","type":"dropShadow"},
-  {"x":0,"y":3,"blur":6,"spread":-4,"color":"#000000 0.12","type":"dropShadow"},
-  {"x":0,"y":9,"blur":28,"spread":8,"color":"#000000 0.05","type":"dropShadow"}],
-
-  focusPrimary: {"x":0,"y":0,"blur":0,"spread":2,"color":"#e6f4ff","type":"dropShadow"},
-  focusError: {"x":0,"y":0,"blur":0,"spread":2,"color":"#fff1f0","type":"dropShadow"},
-
-  focusWarning: {"x":0,"y":0,"blur":0,"spread":2,"color":"#fffbe6","type":"dropShadow"}
-
-*/
-
  }
