25400dd2232112bb84c05d332a301a10a073455f
Crross <cristobalromerorossi@gmail.com>
2023-09-07 08:54:12 -0500

    rename elevation tokens

--- themes/shadows.ts
+++ themes/shadows.ts
@@ -1,11 +1,8 @@
 export const shadows = {
-
-  evaluationLow: "0px 0px 8px 0px #00000014, 0px 1px 2px -1px #0000000a, 0px 2px 7px 2px #00000005",
-  evaluationMiddle: "0px 0px 8px 0px #0000000a, 0px 2px 3px -2px #0000000f, 0px 4px 14px 4px #00000008",
-  evaluationHigh: "0px 0px 16px 0px #00000014, 0px 3px 6px -4px #0000001f, 0px 9px 28px 8px #0000000d",
+  elevationLow: "0px 0px 8px 0px #00000014, 0px 1px 2px -1px #0000000a, 0px 2px 7px 2px #00000005",
+  elevationMiddle: "0px 0px 8px 0px #0000000a, 0px 2px 3px -2px #0000000f, 0px 4px 14px 4px #00000008",
+  elevationHigh: "0px 0px 16px 0px #00000014, 0px 3px 6px -4px #0000001f, 0px 9px 28px 8px #0000000d",
   focusPrimary: "0px 0px 0px 2px #e6f4ff",
   focusError: "0px 0px 0px 2px #fff1f0",
   focusWarning: "0px 0px 0px 2px #fffbe6",
-
-
  }
