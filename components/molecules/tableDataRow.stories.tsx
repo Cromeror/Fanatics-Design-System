9a55acda5808003cdbe5b68837072b0b2be67987
Crross <cristobalromerorossi@gmail.com>
2023-09-07 11:25:36 -0500

    update tableRow example

--- components/molecules/tableDataRow.stories.tsx
+++ components/molecules/tableDataRow.stories.tsx
@@ -3,6 +3,8 @@ import {Meta} from "@storybook/react";
 import {TableDataRow} from "./TableDataRow";
 import {StoryObj} from "@storybook/react";
 import {RowItem} from "../atoms/rowItem/RowItem";
+import Button from "../atoms/button";
+import {DeleteOutlined, EditOutlined} from "@ant-design/icons";
 
 const meta: Meta = {
   title: "Molecules/TableDataRow",
@@ -33,7 +35,14 @@ export const Default: Story = {
         <RowItem>Table cell text</RowItem>
         <RowItem>Table cell text</RowItem>
         <RowItem>Table cell text</RowItem>
-        <RowItem>Table cell text</RowItem>
+        <RowItem>
+          <Button type={"link"} size={"small"} icon={true}>
+            <EditOutlined/>
+          </Button>
+          <Button type={"link"} size={"small"} icon={true}>
+            <DeleteOutlined/>
+          </Button>
+        </RowItem>
       </>
     ),
   },
@@ -46,7 +55,14 @@ export const Selected: Story = {
         <RowItem>Table cell text</RowItem>
         <RowItem>Table cell text</RowItem>
         <RowItem>Table cell text</RowItem>
-        <RowItem>Table cell text</RowItem>
+        <RowItem>
+          <Button type={"link"} size={"small"} icon={true}>
+            <EditOutlined/>
+          </Button>
+          <Button type={"link"} size={"small"} icon={true}>
+            <DeleteOutlined/>
+          </Button>
+        </RowItem>
       </>
     ),
     selected: true
